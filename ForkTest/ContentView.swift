2c9ad6c286a1743bd975633a962af00e617f4083
Indading <baek@indading.com>
2025-01-17 10:46:34 +0900

    Initial Commit

---
+++ ForkTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ForkTest
+//
+//  Created by KyoungJin Baek on 1/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ForkTest/ForkTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForkTestApp.swift
+//  ForkTest
+//
+//  Created by KyoungJin Baek on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ForkTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
